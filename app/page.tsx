9389eb2bf317806be6c762ab6122d0c6f1eadc19
MangoRox <abdullahshaheed46@gmail.com>
2024-02-19 16:43:22 -0500

    added placeholder for left and right panel

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import NavigationBar from "@/components/NavigationBar";
 import Image from "next/image";
+import LeftPanel from "@/components/LeftPanel";
+import RightPanel from "@/components/RightPanel";
 
 export default function Page() {
   return (
     <main>
       <div className="flex-auto">
         <div className="min-w-50" id="panel1">
-          Put left panel component here.
+          <LeftPanel/>
         </div>
         <div className="min-w-50" id="panel2">
-          Put right panel here.
+          <RightPanel/>
         </div>
       </div>
     </main>
