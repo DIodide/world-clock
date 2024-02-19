f9f49365ea10bcb4686582317fae2c4215a6ce52
DIodide <ibraheem.amin3@gmail.com>
2024-02-19 16:35:51 -0500

    add boilerplate structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import NavigationBar from "@/components/NavigationBar";
 import Image from "next/image";
 
-export default function Home() {
-  return <></>;
+export default function Page() {
+  return (
+    <main>
+      <div className="flex-auto">
+        <div className="min-w-50" id="panel1">
+          Put left panel component here.
+        </div>
+        <div className="min-w-50" id="panel2">
+          Put right panel here.
+        </div>
+      </div>
+    </main>
+  );
 }
