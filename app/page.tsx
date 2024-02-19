667ee5990dfa1f02a13f4af2fa13e11c64dec2e7
DIodide <ibraheem.amin3@gmail.com>
2024-02-19 16:56:28 -0500

    Add flex grow

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import RightPanel from "@/components/RightPanel";
 export default function Page() {
   return (
     <main>
-      <div className="flex-auto flex-wrap">
+      <div className="flex flex-wrap">
         <LeftPanel className="min-w-[30vw] bg-red-300" id="panel1" />
         <RightPanel className="min-w-[70vw] bg-blue-300" id="panel2" />
       </div>
